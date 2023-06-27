313daccc31ffeae5a303542c5ec192a75154976d
Rafael Lannes <rafaelvascolannes@gmail.com>
2023-06-27 17:38:58 -0300

    usuarios online vuex

--- resources/js/Echo.js
+++ resources/js/Echo.js
@@ -1,16 +1,24 @@
+import store from './vuex/store'
+
 export default function () {
     window.Echo.join('laravelchat_database_chatroom')
         .here((users) => {
             console.log('Usuarios online');
             console.log(users);
+
+            store.commit('ADD_ONLINE_USERS', users);
         })
         .joining((user) => {
             console.log('Usuario conectado');
             console.log(user);
+
+            store.commit('ADD_ONLINE_USER', user);
         })
         .leaving((user) => {
             console.log('Usuario saiu');
             console.log(user);
+
+            store.commit('REMOVE_ONLINE_USER', user);
         })
 }
 
--- resources/js/vuex/modules/users/state.js
+++ resources/js/vuex/modules/users/state.js
@@ -1,5 +1,7 @@
 export default {
     users: {
         data: [],
-    }
+    },
+
+    onlineUsers: [],
 }
